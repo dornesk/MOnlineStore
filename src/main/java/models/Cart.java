ff867e36553899556cbd6ecdd566842a1dd025e2
dornesk <dornesk@icloud.com>
2025-06-26 10:13:16 +0300

    fix: prevent negative quantity, duplicate items in cart by merging quantities in Cart.addItem

--- src/main/java/models/Cart.java
+++ src/main/java/models/Cart.java
@@ -8,7 +8,19 @@ public class Cart {
     private double discountPercent = 0.0;
 
     public void addItem(Product product, int quantity) {
-        items.add(new CartItem(product, quantity));
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Количество должно быть больше 0");
+        }
+
+        items.stream()
+                .filter(n -> n.getProduct().name().equalsIgnoreCase(product.name()))
+                .findFirst()
+                .ifPresentOrElse(
+                        existing -> //если нашли - увеличиваем количество
+                                existing.setQuantity(existing.getQuantity()+quantity),
+                        () -> //не нашли - добавляем новый товар
+                                items.add(new CartItem(product, quantity))
+                );
     }
 
     public List<CartItem> getItems() {
