051981008af6d5cc7a57c618501a6e0f106ee312
dornesk <dornesk@icloud.com>
2025-06-26 09:42:32 +0300

    test: added tests for addProductToCart, applyDiscount

--- src/test/java/services/StoreServiceTest.java
+++ src/test/java/services/StoreServiceTest.java
@@ -1,14 +1,25 @@
 package services;
 
+import models.Product;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class StoreServiceTest {
     private StoreService storeService;
 
     @BeforeEach
     void setUp() {
+        List<Product> catalog = new ArrayList<>();
+        storeService = new StoreService(catalog);
+        catalog.add(new Product("shirt", 1000));
+        storeService.addProductToCart("shirt", 2);
     }
 
     @Test
@@ -26,19 +37,31 @@ class StoreServiceTest {
     @Test
     @DisplayName("Корректно добавляет товар в каталог")
     void addProductToCart_shouldAddProductToCart() {
-
+        storeService.addProductToCart("shirt", 1);
+        assertEquals(1, storeService.getCartItems().size());
     }
 
     @Test
     @DisplayName("Выбрасывает исключение при количестве <=0")
     void addProductToCart_shouldThrowIllegalArgException() {
-
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                storeService.addProductToCart("shirt", -1));
     }
 
     @Test
     @DisplayName("Корректное применение скидки")
     void applyDiscount_shouldApplyDiscount() {
+        storeService.applyDiscount(25);
+        double total = storeService.calculateTotal();
 
+        assertEquals(1500, total);
+    }
+
+    @Test
+    @DisplayName("Корректное применение скидки")
+    void applyDiscount_shouldThrowIllegalArgException() {
+        assertThrows(IllegalArgumentException.class, () -> storeService.applyDiscount(250));
+        assertThrows(IllegalArgumentException.class, () -> storeService.applyDiscount(-5));
     }
 
     @Test
